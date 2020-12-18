257e7e00bb2be3852052b5c7f3bf070397ad25c5
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2020-12-18 04:10:29 +0200

    Validating and processing a menu items collection before save

--- models/Menu.php
+++ models/Menu.php
@@ -9,6 +9,7 @@ use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
 use yii\behaviors\TimestampBehavior;
 use yii\behaviors\BlameableBehavior;
+use wdmg\validators\JsonValidator;
 use wdmg\base\behaviors\SluggableBehavior;
 use wdmg\base\models\ActiveRecord;
 use wdmg\menu\models\MenuItems;
@@ -97,6 +98,7 @@ class Menu extends ActiveRecord
             [['name', 'alias'], 'string', 'min' => 3, 'max' => 64],
             ['description', 'string', 'max' => 255],
             ['status', 'integer'],
+            ['items', JsonValidator::class, 'message' => Yii::t('app/modules/menu', 'The value of field `{attribute}` must be a valid JSON, error: {error}.')],
             ['status', 'in', 'range' => array_keys($this->getStatusesList(false))],
             ['alias', 'match', 'skipOnEmpty' => true, 'pattern' => '/^[A-Za-z0-9\-\_]+$/', 'message' => Yii::t('app/modules/menu','It allowed only Latin alphabet, numbers and the «-», «_» characters.')],
             [['created_at', 'updated_at'], 'safe'],
@@ -128,6 +130,21 @@ class Menu extends ActiveRecord
         ];
     }
 
+
+    /**
+     * {@inheritdoc}
+     */
+    public function beforeValidate()
+    {
+        /*if (is_string($this->items) && JsonValidator::isValid($this->items)) {
+            $this->items = \yii\helpers\Json::decode($this->items);
+        } elseif (is_array($this->items)) {
+            $this->items = \yii\helpers\Json::encode($this->items);
+        }*/
+
+        return parent::beforeValidate();
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -136,6 +153,13 @@ class Menu extends ActiveRecord
 
         if (is_string($this->status))
             $this->status = intval($this->status);
+
+        if (is_string($this->items) && JsonValidator::isValid($this->items)) {
+            $this->items = \yii\helpers\Json::decode($this->items);
+            $menuItems = \wdmg\helpers\ArrayHelper::flattenTree($this->items, 'children', 'parent_id');
+            var_export($menuItems);
+            die();
+        }
 
         return parent::beforeSave($insert);
     }
